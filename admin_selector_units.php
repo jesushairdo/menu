0880714491ab7858670c10fde38ae34b1e1c589d
Jamie Kerwick <jamie@jamiekerwick.co.uk>
2017-09-04 11:12:25 +0100

    correction

--- admin_selector_units.php
+++ admin_selector_units.php
@@ -2,7 +2,7 @@
 require_once('include.inc.php');
 $smarty = new Smarty_Menu();
 
-$smarty->assign('selectedUnit', '--new--');
+$smarty->assign('selectedUnit', '1');
 $options = array('0'=>'option 1', '1'=>'option 2');
 $smarty->assign('unitOptions', $options);
 $smarty->display('admin_selector_units.tpl');
