d4d7334e43ae23c01cdb75caf2a5fe79f8b84ef2
Jamie <Jamie@jamiekerwick.co.uk>
2017-09-04 20:41:48 +0100

    removed database files

--- setup.php
+++ setup.php
@@ -1,32 +1,32 @@
 <?php
-	date_default_timezone_set('UTC');	
-	try 
+	date_default_timezone_set('UTC');
+	try
 	{
 		// create database and open connections
 		// Create (connect to SQLite Database in file)
 		$fileDb = new PDO('sqlite:food.sqlite3');
 		//set erromode to exceptions
 		$fileDb->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
+
 		//create tables
 		//enable foreign keys
 		$fileDb->exec('PRAGMA foreign_keys=on');
 		//create units table
-		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblUnits (unitId INTEGER PRIMARY KEY, unitName TEXT)');
+		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblUnits (unitId INTEGER PRIMARY KEY, unitName TEXT unique)');
 		//create Ingredients table
-		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblIngredients (ingredientId INTEGER PRIMARY KEY, ingredientName TEXT)');
+		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblIngredients (ingredientId INTEGER PRIMARY KEY, ingredientName TEXT unique)');
 		//create workouts table
-		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblWorkOuts (workoutId INTEGER PRIMARY KEY, workoutName TEXT, workoutLocation TEXT, workoutReference TEXT)');
+		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblWorkOuts (workoutId INTEGER PRIMARY KEY, workoutName TEXT unique, workoutLocation TEXT, workoutReference TEXT)');
 		//create recipes table
-		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblRecipes (recipeId INTEGER PRIMARY KEY, recipeName TEXT, recipeLocation TEXT, recipeReference TEXT)');
+		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblRecipes (recipeId INTEGER PRIMARY KEY, recipeName TEXT unique, recipeLocation TEXT, recipeReference TEXT)');
 		//create recipeIngredients table
 		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblRecipeIngredients (recipeId INTEGER, ingredientId INTEGER, amount TEXT, unitId INTEGER, PRIMARY KEY (recipeId, ingredientId), FOREIGN KEY (recipeId) REFERENCES tblRecipes(recipeId) ON UPDATE CASCADE, FOREIGN KEY(ingredientId) REFERENCES tblIngredients(ingredientId) ON UPDATE CASCADE) WITHOUT ROWID');
 		//create menus table
 		$fileDb->exec('CREATE TABLE IF NOT EXISTS tblMenus (menuId INTEGER PRIMARY KEY, startdate INTEGER, meal1 INTEGER, meal2 INTEGER, meal3 INTEGER, workout1 INTEGER, workout2 INTEGER, workout3 INTEGER, FOREIGN KEY(meal1) REFERENCES tblRecipes(recipeId) ON UPDATE CASCADE, FOREIGN KEY(meal2) REFERENCES tblRecipes(recipeId) ON UPDATE CASCADE, FOREIGN KEY(meal3) REFERENCES tblRecipes(recipeId) ON UPDATE CASCADE, FOREIGN KEY(workout1) REFERENCES tblWorkOuts(workoutId) ON UPDATE CASCADE, FOREIGN KEY(workout2) REFERENCES tblWorkOuts(workoutId) ON UPDATE CASCADE, FOREIGN KEY(workout3) REFERENCES tblWorkOuts(workoutId) ON UPDATE CASCADE)');
-		
-		/*		
+
+		/*
 		// set initial data
-		$messages = array( 
+		$messages = array(
 			array('title'=>'Hello!', 'message'=> 'Just Testing....', 'time' => 1327301464),
 			array('title'=>'Hello Again!', 'message'=>'More testing....', 'time'=>1339428612),
 			array('title'=>'Hi!', 'message'=>'SQLite3 is cool...', 'time'=>1327214268)
@@ -34,23 +34,23 @@
 		// prep insert statement
 		$insert = "INSERT INTO messages (title,message,time) VALUES (:title, :message, :time)";
 		$stmt = $fileDb->prepare($insert);
-		
+
 		//bind parameters to statement variables
 		$stmt->bindParam(':title', $title);
 		$stmt->bindParam(':message', $message);
 		$stmt->bindParam(':time', $time);
-		
+
 		//loop thru all messages and execute prepared insert statement
 		foreach ($messages as $m)
 		{
 			$title=$m['title'];
 			$message=$m['message'];
 			$time=$m['time'];
-			
+
 			//Execute Statement
 			$stmt->execute();
 		}
-		
+
 		//select all data from file db messages table
 		$result = $fileDb->query('Select * FROM messages');
 		foreach ($result as $row)
