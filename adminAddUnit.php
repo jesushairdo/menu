f135e5297060f9fbb932cd25880902864514502d
Jamie Kerwick <jamie@jamiekerwick.co.uk>
2017-09-04 13:23:29 +0100

    fix form

--- adminAddUnit.php
+++ adminAddUnit.php
@@ -1,8 +1,15 @@
 <?php
 $smarty->assign('formAction', 'doAdminAddUnit');
-
+if (array_key_exists('unitName', $_POST))
+{
+    $unitName = $_POST['unitName'];
+}
+else
+{
+    $unitName = '';
+}
 //$smarty->assign('unitId', $row->unitId);
-//$smarty->assign('unitName', $row->unitName);
+$smarty->assign('unitName', $unitName);
 $smarty->assign('contentTemplate', 'adminAddUnit.tpl.html');
 $smarty->assign('pageContentTitle', 'Add Unit');
 ?>
