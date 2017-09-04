bf09ac55ed355ceaaa87c86e3989db1c0afd6d00
Jamie Kerwick <jamie@jamiekerwick.co.uk>
2017-09-04 11:36:21 +0100

    sub-templating

--- adminSelectorUnits.php
+++ adminSelectorUnits.php
@@ -1,9 +1,6 @@
 <?php
-require_once('include.inc.php');
-$smarty = new Smarty_Menu();
-
 $smarty->assign('selectedUnit', '1');
 $options = array('0'=>'option 1', '1'=>'option 2');
 $smarty->assign('unitOptions', $options);
-$smarty->display('admin_selector_units.tpl');
+$smarty->assign('contentTemplate', 'adminSelectorUnits.tpl.html');
 ?>
--- index2.php
+++ index2.php
@@ -2,7 +2,26 @@
 require_once('include.inc.php');
 
 $smarty = new Smarty_Menu();
-$smarty->assign('placeholder', 'value');
 
-$smarty->display('template.tpl');
+if ($_SERVER['REQUEST_METHOD'] =='POST')
+{
+    $action = $_POST['action'];
+}
+else
+{
+    $action = $_GET['action'];
+}
+switch($action)
+{
+    case 'adminSelectorUnits':
+        require_once('adminSelectorUnits.php');
+        break;
+    default:
+        //require_once('welcome.php');
+        break;
+}
+
+$smarty->assign('placeholder', 'value');
+//display resulting page to user
+$smarty->display('fullPage.tpl.html');
 ?>
