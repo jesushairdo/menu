f12c9492346ec8c47bce439ceab02944f8dec637
Jamie Kerwick <jamie@jamiekerwick.co.uk>
2017-09-04 12:16:42 +0100

    debugging info

--- index2.php
+++ index2.php
@@ -21,21 +21,27 @@ print '<!-- action: '. $action . ' -->';
 switch($action)
 {
     case 'adminSelectorUnits':
+        print '<!-- performing adminSelectorUnits -->';
         require_once('adminSelectorUnits.php');
         break;
     case 'adminEditUnit':
+        print '<!-- performing adminEditUnit -->';
         require_once('adminEditUnit.php');
         break;
     case 'doAdminEditUnit':
+        print '<!-- performing doAdminEditUnit -->';
         require_once('doAdminEditUnit.php');
         break;
     case 'adminAddUnit':
+        print '<!-- performing adminAddUnit -->';
         require_once('adminAddUnit.php');
         break;
     case 'doAdminAddUnit':
+        print '<!-- performing doAdminAddUnit -->';
         require_once('doAdminAddUnit.php');
         break;
     default:
+        print '<!-- performing default -->';
         require_once('welcome.php');
         break;
 }
