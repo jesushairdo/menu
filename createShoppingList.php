11239672c57417673456b02749eaa04553a99bcb
Jamie Kerwick <jamie@jamiekerwick.co.uk>
2017-09-05 15:54:46 +0100

    merge all menus into 1 sql statement

--- createShoppingList.php
+++ createShoppingList.php
@@ -1,12 +1,13 @@
 <?php
 //create shopping list from menus created.
 // need to workout whether to choose a list of menus, or use a start date / end date, OR do i create another object that represents the full week?
-//prepare the statements
+//prepare the statements for the obtaining of menu information
 $menuSql = 'SELECT tblMenus.meal1, tblMenus.meal2, tblMenus.meal3 FROM tblMenus WHERE tblMenus.menuId = :menuId';
 $menuStmt = $db->prepare($menuSql);
 $menuStmt->bindParam(':menuId', $menuId);
 $menuIds[] = 1;
 $menuIds[] = 2;
+$unionSql = array();
 //obtain the recipeIds for the menus required
 foreach ($menuIds as $menuId)
 {
@@ -14,23 +15,25 @@ foreach ($menuIds as $menuId)
     $menuStmt->execute();
     $menus = $menuStmt->fetch(); // can only return one, as we are searching by menuId
     //$allMenus = array();
-    $allMenus[] = $menus;
+    //$allMenus[] = $menus;
+    $allRecipes[] = $menus->meal1;
+    $allRecipes[] = $menus->meal2;
+    $allRecipes[] = $menus->meal3;
 }
+//prepare the union SQL
+for($i=0; $i<count($allRecipes); $i++)
+{
+    $unionSql[] = 'SELECT  tblRecipeIngredients.amount AS amount, tblRecipeIngredients.unitId AS unitId, tblRecipeIngredients.ingredientId FROM tblRecipeIngredients WHERE tblRecipeIngredients.RecipeId = '.$allRecipes[$i];
+}
+$unionSql = implode(' UNION ALL ', $unionSql);
 //prepare the SQL to obtain the ingredient list for this menu
-$ingredientsSql = 'SELECT SUM(IngredientList.amount) AS amount, tblUnits.unitName AS unit, tblIngredients.ingredientName AS ingredient FROM (
-SELECT  tblRecipeIngredients.amount AS amount, tblRecipeIngredients.unitId AS unitId, tblRecipeIngredients.ingredientId FROM tblRecipeIngredients WHERE tblRecipeIngredients.RecipeId = :meal1
-UNION ALL
-SELECT  tblRecipeIngredients.amount AS amount, tblRecipeIngredients.unitId AS unitId, tblRecipeIngredients.ingredientId FROM tblRecipeIngredients WHERE tblRecipeIngredients.RecipeId = :meal2
-UNION ALL
-SELECT  tblRecipeIngredients.amount AS amount, tblRecipeIngredients.unitId AS unitId, tblRecipeIngredients.ingredientId FROM tblRecipeIngredients WHERE tblRecipeIngredients.RecipeId = :meal3
-) AS IngredientList
+$ingredientsSql = 'SELECT SUM(IngredientList.amount) AS amount, tblUnits.unitName AS unit, tblIngredients.ingredientName AS ingredient FROM ('.$unionSql.') AS IngredientList
 LEFT JOIN tblUnits ON tblUnits.unitId = IngredientList.unitId
 LEFT JOIN tblIngredients ON tblIngredients.ingredientId = IngredientList.ingredientId
 GROUP BY ingredient,unit';
+$ingredients = $db->query($ingredientsSql)->fetchAll();
+/*
 $ingredientsStmt = $db->prepare($ingredientsSql);
-$ingredientsStmt->bindParam(':meal1', $meal1);
-$ingredientsStmt->bindParam(':meal2', $meal2);
-$ingredientsStmt->bindParam(':meal3', $meal3);
 //print_r($allMenus);
 foreach ($allMenus as $menu)
 {
@@ -45,6 +48,7 @@ foreach ($allMenus as $menu)
         $ingredients[] = $result;
     }
 }
+*/
 print_r($ingredients);
 die();
 /*
